a307cdf98334c93af3f655ae85a21ae49ded0a6c
Yoojin Kim <yoojinkim@Yoojinui-MacBookAir.local>
2022-02-08 22:37:12 +0900

    add: images & code

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let scrollTop = 0;
+let imageAll;
+let totalNum = 0;
+
+window.onload = function() {
+    imageAll = document.querySelectorAll(".parallax_image");
+    totalNum = imageAll.length;
+
+    window.addEventListener('scroll', scrollFunc);
+}
+
+function scrollFunc(e) {
+    scrollTop = this.scrollY;
+
+    if(scrollTop > 0)
+        for(var i=0; i< totalNum ; i++){
+            imageAll[i].style.transform = "perspective(500px) translateZ("+ scrollTop/(2*(totalNum-i)) +"px)";
+            // console.log(scrollTop , scrollTop / (5*(totalNum-i)) );
+        }
+
+}
+
+
