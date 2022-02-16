74bd25c1b8c2b648eac4cc0705262488cc112998
Yoojin Kim <yoojinkim@Yoojinui-MacBookAir.local>
2022-02-17 00:58:22 +0900

    수정: 안내 문구 추가

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let totalNum = 0;
 
 window.onload = function() {
     imageAll = document.querySelectorAll(".parallax_image");
+    infoText = document.querySelector(".info-wrapper");
     totalNum = imageAll.length;
 
     window.addEventListener('scroll', scrollFunc);
@@ -12,12 +13,19 @@ window.onload = function() {
 function scrollFunc(e) {
     scrollTop = this.scrollY;
 
-    if(scrollTop > 0)
+    if(scrollTop > 0) {
         for(var i=0; i< totalNum ; i++){
             imageAll[i].style.transform = "perspective(500px) translateZ("+ scrollTop/(2*(totalNum-i)) +"px)";
             // console.log(scrollTop , scrollTop / (5*(totalNum-i)) );
         }
 
+        // infoText.style.transform = "perspective(500px) translateZ("+ scrollTop/(2*(totalNum)) +"px)";
+    } else {
+        for(var i=0; i< totalNum ; i++){
+            imageAll[i].style.transform = "translateZ(0px)";
+            // console.log(scrollTop , scrollTop / (5*(totalNum-i)) );
+        }
+    }
 }
 
 
